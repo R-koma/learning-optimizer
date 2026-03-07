2aacf4009e325030d4f5fe2fdfbe0aa03356fce3
R-koma <ryomakomazawa@gmail.com>
2026-03-07 23:51:07 +0900

    feat: implement BetterAuth credential authentication

---
+++ client/app/(main)/dashboard/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+
+export default function DashBoard() {
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+
+  if (isPending) return <p>読み込み中...</p>;
+  if (!session) return <p>ログインしてください。</p>;
+
+  const handleSignOut = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  };
+  return (
+    <div>
+      <h1>DashBoard</h1>
+      <p>{session.user.name}</p>
+      <button onClick={handleSignOut}>ログアウト</button>
+    </div>
+  );
+}
